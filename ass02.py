c64ed88caf2cdc106ff56124073c6ee4d570f916
himasnhu <negi1049@gmail.com>
2018-06-19 19:46:37 +0530

    2nd assignment

---
+++ ass02.py
@@ -0,0 +1,29 @@
+#print something
+print("hello world")
+
+#join string
+c="hello"
+d="python"
+print(c + d)
+
+#print variable values
+c=5
+d=4
+e=3
+print(c,d,e)
+
+#lets get started
+
+print("lets get started")
+
+#place holder
+s="acadview"
+course="pyhton"
+fees=5000
+print("institute=",s,"course=",course,"fees=",fees)
+
+#lets do somethng intersting
+name="tony stark"
+salary=150000
+print("name=",name)
+print("salary=",salary)
